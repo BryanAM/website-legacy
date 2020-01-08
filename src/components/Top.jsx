487e665fa2db43ffd85fe7db90a33bb8364e7466
Bryan <bryaument@gmail.com>
2020-01-07 22:54:39 -0500

    added header component and some basic styles

---
+++ src/components/Top.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../resources/styles/top.scss';
+
+const Top = () => {
+  return (
+    <header>
+      <div className="top">
+        <p className="me">Bryan Aument's</p>
+      </div>
+    </header>
+  );
+};
+export default Top;
