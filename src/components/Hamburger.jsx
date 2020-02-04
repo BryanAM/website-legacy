57b368834fb6cde9ce2d5fe11c654e653824a225
Bryan <bryaument@gmail.com>
2020-02-03 21:56:57 -0500

    small update to hamburger svg

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -11,9 +11,9 @@ const Hamburger = () => {
   return(
     <button id="onTheMenu" className="hamburger-menu" type="button" onClick={handleOnClick}>
       <svg viewBox="0 0 102 80" width="40" height="40" fill="#ffffc4">
-        <rect className={ open? "top-bar" : null } width="80" height="15" rx="10"></rect>
-        <rect className="middle-bard" y="30" width="90" height="15" rx="8"></rect>
-        <rect className={ open? "bottom-bar": null} y="60" width="100" height="15" rx="8"></rect>
+        <rect className={ open? "top-bar" : null } width="80" height="10" rx="10"></rect>
+        <rect className="middle-bard" y="30" width="90" height="10" rx="8"></rect>
+        <rect className={ open? "bottom-bar": null} y="60" width="100" height="10" rx="8"></rect>
       </svg>
     </button>
   );
