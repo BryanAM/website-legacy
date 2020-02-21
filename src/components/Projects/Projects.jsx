c195009edf60e441396bb832c7bf58eba43ec19d
Bryan <bryaument@gmail.com>
2020-02-20 22:27:33 -0500

    added projectstiles and some basic styles

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import ProjectTile from '../Tiles/ProjectTile/ProjectTile';
+import { useTranslation } from 'react-i18next';
 
 const Projects = () => {
+  const [t] = useTranslation();
   return(
     <section  id="projects-section" className="section">
+      <h2 className="section-header">{t('projects.header')}</h2>
+      <ProjectTile />
     </section>
   );
 };
---
+++ src/components/Tiles/ProjectTile/ProjectTile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './projectTile.scss';
+
+const ProjectTile = () => {
+  return(
+    <a href="/" className="project-tile">
+    </a>
+  ); 
+};
+export default ProjectTile;
