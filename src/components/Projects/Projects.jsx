55be58455d89323221e8e3b7058ed701bca6c8f5
Bryan <bryaument@gmail.com>
2020-03-14 16:35:36 -0400

    update axios and gitignore

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ProjectTile from '../Tiles/ProjectTile/ProjectTile';
 import { useTranslation } from 'react-i18next';
+import axios from 'axios';
 
 const Projects = () => {
   const [t] = useTranslation();
+  const axiosGitHubGraphQL = axios.create({
+    baseURL: 'https://api.github.com/graphql',
+    headers: {
+      Authorization: 'bearer ....'
+    }
+  });
+
   return(
-    <section  id="projects-section" className="section">
-      <h2 className="section-header">{t('projects.header')}</h2>
+    <section  id='projects-section' className='section'>
+      <h2 className='section-header'>{t('projects.header')}</h2>
       <ProjectTile />
     </section>
   );
