5078738ba503d8d3fe59fcc274ad4572b4ff8812
Bryan <bryaument@gmail.com>
2020-03-14 16:53:22 -0400

    added simple axios call boiler code

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProjectTile from '../Tiles/ProjectTile/ProjectTile';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
 
 const Projects = () => {
+  const [gitHubData, setGitHubData] = useState({});
   const [t] = useTranslation();
   const axiosGitHubGraphQL = axios.create({
     baseURL: 'https://api.github.com/graphql',
     headers: {
-      Authorization: 'bearer ....'
+      Authorization: `bearer ${
+        process.env.REACT_APP_GITHUB_ACCESS_TOKEN
+      }`,
     }
+  });
+
+  // API Call to get current repo info
+  useEffect(() => {
+    
   });
 
   return(
