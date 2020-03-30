8d5be71520e88e2a2e65c459b0a4daacc1039b16
Bryan <bryaument@gmail.com>
2020-03-30 12:45:36 -0400

    added connect button

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -6,8 +6,10 @@ const Connect = () => {
 
   return(
     <section id='connect-section'>
+      <h2 className='section-header'>{t('connect.header')}</h2>
       <h2 className='connect-title'>{t('connect.me')}</h2>
       <p className='connect-employed'>{t('connect.employed')}</p>
+      <button className='connect-hi'>{t('connect.hi')}</button>
     </section>
   );
 };
