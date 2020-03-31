026ff04ef2995503861fbfec5e623eeeb9d20b95
Bryan <bryaument@gmail.com>
2020-03-30 23:35:15 -0400

    update styles for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,6 @@ import './footer.scss';
 const Footer = () => {
   return(
     <footer id='footer'>
-      <p></p>
       <a className='footer-link-icon' href='https://github.com/BryanAM' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
       <a className='footer-link-icon' href='https://www.linkedin.com/in/bryanaument/' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
     </footer>
