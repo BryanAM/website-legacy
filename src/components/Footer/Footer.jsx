c883829c7e103fc5c2d7b3263f5dbb6f953fdba3
Bryan <bryaument@gmail.com>
2020-03-30 23:10:21 -0400

    added some fonts and links for github and linkedin

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import './footer.scss';
 
 const Footer = () => {
   return(
     <footer id='footer'>
+      <p></p>
+      <a className='footer-link-icon' href='https://github.com/BryanAM' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+      <a className='footer-link-icon' href='https://www.linkedin.com/in/bryanaument/' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
     </footer>
   );
 };
