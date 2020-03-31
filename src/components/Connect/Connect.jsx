39979662785e8e4662678ff3edabd76e375de888
Bryan <bryaument@gmail.com>
2020-03-30 21:23:29 -0400

    added on click event to handle email in connection section

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -3,13 +3,15 @@ import { useTranslation } from 'react-i18next';
 import './connect.scss';
 const Connect = () => {
   const [t] =useTranslation();
-
+  const handleOnClick = () => {
+    window.location = "mailto:bryaument@gmail.com?subject=I saw your website! I'm saying hi! ";
+  }
   return(
     <section id='connect-section'>
       <h2 className='section-header'>{t('connect.header')}</h2>
       <h2 className='connect-title'>{t('connect.me')}</h2>
       <p className='connect-employed'>{t('connect.employed')}</p>
-      <button className='connect-hi'>{t('connect.hi')}</button>
+      <button className='connect-hi' onClick={handleOnClick}>{t('connect.hi')}</button>
     </section>
   );
 };
