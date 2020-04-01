1e9b0094f9b76c1f910f27674badb53c764441d3
Bryan <bryaument@gmail.com>
2020-03-31 22:34:07 -0400

    added some footer text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,14 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import './footer.scss';
 
 const Footer = () => {
+  const [t] = useTranslation();
   return(
     <footer id='footer'>
-      <a className='footer-link-icon' href='https://github.com/BryanAM' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
-      <a className='footer-link-icon' href='https://www.linkedin.com/in/bryanaument/' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+      <p className='made-by'>{t('footer.madeBy')}</p>
+      <div className='icons'>
+        <a className='footer-link-icon' href='https://github.com/BryanAM' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+        <a className='footer-link-icon' href='https://www.linkedin.com/in/bryanaument/' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+      </div>
     </footer>
   );
 };
