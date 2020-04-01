dbae401af95183f3957fb7ba54da8d0cf759fe5a
Bryan <bryaument@gmail.com>
2020-03-31 23:39:05 -0400

    set up choose language button for welcome, needs some styles and logic

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import i18n from '../../i18n/index.js';
 import './welcome.scss';
 
 const Welcome = () => {
   const [t] = useTranslation();
+  //TODO need to add some fancy logic for choosing the opposite language
+  const handleOnClick = () => {
+    i18n.changeLanguage('ja', (err, t) => {
+  if (err) return console.log('something went wrong loading', err);
+  t('key'); // -> same as i18next.t
+});
+  }
   return(
     <section id="welcome-section" className="section">
       <h1 className="section-header">{t('welcome.greeting')}</h1>
       <p className="me-name">{t('welcome.name')}</p>
       <p className="me-short">{t('welcome.about')}</p>
       <p className="me-long">{t('welcome.detail')}</p>
+      <button onClick={handleOnClick}></button>
     </section>
   );
 
