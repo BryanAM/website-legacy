9215d4ec5aa02311f618507a0c32a88231b84afa
Bryan <bryaument@gmail.com>
2020-04-01 20:56:35 -0400

    implemented language toggle switch

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -5,20 +5,23 @@ import './welcome.scss';
 
 const Welcome = () => {
   const [t] = useTranslation();
-  //TODO need to add some fancy logic for choosing the opposite language
+
   const handleOnClick = () => {
-    i18n.changeLanguage('ja', (err, t) => {
-  if (err) return console.log('something went wrong loading', err);
-  t('key'); // -> same as i18next.t
-});
+    let switchLang = i18n.language === 'ja' ? 'en' : 'ja';
+    i18n.changeLanguage(switchLang, (err, t) => {
+      if (err) return console.log('something went wrong loading', err);
+    });
   }
   return(
-    <section id="welcome-section" className="section">
-      <h1 className="section-header">{t('welcome.greeting')}</h1>
-      <p className="me-name">{t('welcome.name')}</p>
-      <p className="me-short">{t('welcome.about')}</p>
-      <p className="me-long">{t('welcome.detail')}</p>
-      <button onClick={handleOnClick}></button>
+    <section id='welcome-section' className='section'>
+      <h1 className='section-header'>{t('welcome.greeting')}</h1>
+      <p className='me-name'>{t('welcome.name')}</p>
+      <p className='me-short'>{t('welcome.about')}</p>
+      <p className='me-long'>{t('welcome.detail')}</p>
+      <p>{t('welcome.switchDesc')}</p>
+      <button className='language-picker' onClick={handleOnClick}>
+        {t('welcome.switch')}
+      </button>
     </section>
   );
 
