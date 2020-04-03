e02a41af0b61d23f0951d6b2915bfc139238770d
Bryan <bryaument@gmail.com>
2020-04-02 21:29:39 -0400

    added some basic hamburger animation

--- src/components/Hamburger/Hamburger.jsx
+++ src/components/Hamburger/Hamburger.jsx
@@ -5,14 +5,14 @@ const Hamburger = () => {
   const [open, setOpen] = useState(false);
 
   const handleOnClick = () => {
+    console.log('clicked!');
     setOpen(!open);
-  };
+    console.log(open);
+    }
+
   return(
-    <button id="onTheMenu" className="hamburger-menu" type="button" onClick={handleOnClick}>
-      <svg viewBox="0 0 102 80" width="40" height="40">
-        <rect className="middle-bard" y="15" width="100" height="10" rx="8"></rect>
-        <rect className="bottom-bar" y="60" width="100" height="10" rx="8"></rect>
-      </svg>
+    <button id='onTheMenu' className='hamburger-menu' type='button' onClick={handleOnClick}>
+      <div className={`menu-burger ${open ? 'open' : 'closed'}`}></div>
     </button>
   );
 };
