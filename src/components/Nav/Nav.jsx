623df4ebee7f488d840ba63da2585e9dfb2bb2d9
Bryan <bryaument@gmail.com>
2020-04-04 11:38:22 -0400

    added basic animations for nav bar list items and email

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import en from '../../i18n/locales/en.json';
 import jp from '../../i18n/locales/jp.json';
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const Nav = () => {
   const [open, setOpen] = useState(false);
@@ -15,21 +15,78 @@ const Nav = () => {
     setOpen(!open);
     }
 
+
+  
+  const navVariant = {
+    open: {
+
+    },
+
+    closed: {
+
+    }
+
+  }
+
+  const ulVariants = {
+    open: {
+      transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+    },
+
+    closed: {
+      transition: { staggerChildren: 0.05, staggerDirection: -1 }
+    }
+  }
+
+  const liVariants = {
+    open: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        y: { stuffness: 1000, velocity: -100 }
+      }
+    },
+
+    closed: {
+      y: 50,
+      opacity: 0,
+      transition: {
+        y: { stuffness: 1000 }
+      }
+    }
+  }
+
   return (
-    <nav id='nav-section' className='nav'>
-      <button className='hamburger' onClick={handleOnClick}><Hamburger open={open}/></button>
-      <a href="mailto:bryaument@gmail.com?subject=I saw your website, let's connect!" ><FontAwesomeIcon icon={faEnvelope} /></a>
-      <div className={`menu ${open ? 'open' : 'closed'}`}>
-        <ul>
-         {Object.keys(en.en.nav).map((value, index) => (
-          <li key={index + 1} className='nav-list-item'> 
-            <a href={`#${en.en.nav[value].id}`} className='nav-item'>{(i18n.language === 'en' ? en.en.nav : jp.jp.nav)[value].desc}</a>
-            <hr align='left'/>
-          </li>
-        ))}
-        </ul>
-      </div>
-    </nav>
+    <motion.nav 
+      id='nav-section'
+      className='nav'
+      initial={false}
+      animate={open ? 'open' : 'closed'}
+    >
+        <button className='hamburger' onClick={handleOnClick}>
+          <Hamburger open={open}/>
+        </button>
+        <motion.a whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} href="mailto:bryaument@gmail.com?subject=I saw your website, let's connect!" >
+          <FontAwesomeIcon icon={faEnvelope} />
+        </motion.a>
+        <motion.div className={`menu ${open ? 'open' : 'closed'}`}>
+          <motion.ul variants={ulVariants}>
+            {Object.keys(en.en.nav).map((value, index) => (
+              <motion.li
+                variants={ liVariants }
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}key={index + 1}
+                className='nav-list-item'
+              > 
+                  <a href={`#${en.en.nav[value].id}`} className='nav-item'>
+                    {(i18n.language === 'en' ? en.en.nav : jp.jp.nav)[value].desc}
+                  </a>
+              <hr  align='left'/>
+            </motion.li>
+          ))}
+          </motion.ul>
+        </motion.div>
+    </motion.nav>
   );
 };
 
