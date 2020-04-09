0f6415d2cd47b29e57fd9166a979b236e5f91494
Bryan <bryaument@gmail.com>
2020-04-09 15:17:51 -0400

    small style changes in work added bullets and color as well as some spacing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import About from './components/About/About.jsx';
 import Nav from './components/Nav/Nav.jsx';
 import Welcome from './components/Welcome/Welcome.jsx';
