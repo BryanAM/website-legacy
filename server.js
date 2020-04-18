5e8e81ac084ed2af47be5f8fff4761e60f4805cf
Bryan <bryaument@gmail.com>
2020-04-17 20:45:32 -0400

    removed nodemon

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send({ express: 'testing express sending some random text' });
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
 });
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
