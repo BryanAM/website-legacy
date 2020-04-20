c3b701f1198f5bcb623c1778e35179a935d78538
Bryan <bryaument@gmail.com>
2020-04-20 10:12:23 -0400

    running tests

---
+++ src/components/Work/work.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import i18n from '../../i18n/index';
+import { shallow } from 'enzyme';
+import Work from './Work.jsx';
+import { I18nextProvider } from 'react-i18next';
+
+describe('About', () => {
+  it('should render without issue', () => {
+    const component = shallow(
+      <I18nextProvider i18n={i18n}>
+        <Work />
+      </I18nextProvider>
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
