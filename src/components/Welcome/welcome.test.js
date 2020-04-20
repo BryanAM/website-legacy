4a5d0fec805bf92ceae07e07c32974427bf7208c
Bryan <bryaument@gmail.com>
2020-04-19 21:13:41 -0400

    added some welcome tests

--- src/components/Welcome/welcome.test.js
+++ src/components/Welcome/welcome.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import i18n from '../../i18n/index.js';
 
 import Welcome from './Welcome.jsx';
 
@@ -8,4 +9,13 @@ describe('Welcome', () => {
     const component = shallow(<Welcome />);
     expect(component).toMatchSnapshot();
   });
+
+  it('should be possible to change the language with button', () => {
+    const component = mount(<Welcome />);
+    let switchLang = i18n.language === 'ja' ? 'en' : 'ja';
+    component
+      .find('button')
+      .simulate('click')
+    expect(switchLang).toBe('ja');
+  })
 });
