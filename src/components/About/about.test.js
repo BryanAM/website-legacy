2f3d9c02619202801d4b195d9e8359b1cd499032
Bryan <bryaument@gmail.com>
2020-04-19 23:11:45 -0400

    test updates

--- src/components/About/about.test.js
+++ src/components/About/about.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import i18n from '../../i18n/index';
 import { shallow, mount } from 'enzyme';
 import About from './About.jsx';
+import { I18nextProvider } from 'react-i18next';
 
 describe('About', () => {
   it('should render without issue', () => {
-    const component = mount(<About />);
+    const component = mount(
+      <I18nextProvider i18n={i18n}>
+        <About />
+      </I18nextProvider>
+    );
     expect(component).toMatchSnapshot();
   });
 });
