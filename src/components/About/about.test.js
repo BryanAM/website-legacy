4135f8a935915738338913289930be6c7c8597b0
Bryan <bryaument@gmail.com>
2020-04-20 10:04:28 -0400

    snapshots added to gitignore

--- src/components/About/about.test.js
+++ src/components/About/about.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import i18n from '../../i18n/index';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import About from './About.jsx';
 import { I18nextProvider } from 'react-i18next';
 
 describe('About', () => {
   it('should render without issue', () => {
-    const component = mount(
+    const component = shallow(
       <I18nextProvider i18n={i18n}>
         <About />
       </I18nextProvider>
